a96cce369876a5fa593a3864fdf7cb5aa706ba01
Felipe Fronchetti <fronchettl@vcu.edu>
2023-04-29 23:49:14 -0400

    More updates

--- js/custom_workspace.js
+++ js/custom_workspace.js
@@ -1,23 +1,15 @@
 mapPositions = new Map();
-
-let checkSubset = (parentArray, subsetArray) => {
-  return subsetArray.every((el) => {
-      return parentArray.includes(el)
-  })
-}
+mapPositions.set("Home", "HOME_POSITION");
 
 /* "Extensions are functions that run on each block of a given type as the block is created." - Blockly */
 /* This extension fill the movement position dropdown with default field values. */
-Blockly.Extensions.register('move_block_fill_options',
+Blockly.Extensions.register('move_block_created',
 function() {
   this
   .getInput('POSITION')
   .appendField(new Blockly.FieldDropdown(
     function() {
       var options = [];
-      options.push(["<somewhere>", 'UNDEFINED']);
-      options.push(["Create Position", "NEW_POSITION"]);
-      options.push(["Home Position", 'HOME_POSITION']);
 
       for (let [mapPosition, mapValue] of mapPositions) {
         options.push([mapPosition, mapValue]);
@@ -27,40 +19,9 @@ function() {
     }), 'DROPDOWN_OPTIONS');
 });
 
-function updateDropdownOptions(dropdownField) {
-    var dropdownOptions = dropdownField.getOptions(false);
-
-    // Push unsaved positions into dropdown options
-    // TODO: Optmize this verification to reduce complexity
-    for (let [mapPosition, mapValue] of mapPositions) {
-      value_exists = false;
-
-      for (let [dropPosition, dropValue] of dropdownOptions) {
-        // If position already exists in dropdown options, break */
-        if (mapValue == dropValue) {
-          value_exists = true;
-          break;
-        }
-      }
-
-      if (!value_exists) {
-        dropdownOptions.push([mapPosition, mapValue]);
-      }
-    }
-
-    dropdownField.menuGenerator_ = dropdownOptions;
-}
-
-function updateBlocks() {
-  Blockly.getMainWorkspace().getBlocksByType("move_to_position").forEach(function(block) {
-    var dropdownField = block.getField('DROPDOWN_OPTIONS');
-    updateDropdownOptions(dropdownField);
-  });  
-}
-
 /* This extension listens to changes in the field value of the movement position dropdown. */
-Blockly.Extensions.register('move_block_warning_on_change', function() {
-  this.setOnChange(function(changeEvent) {
+Blockly.Extensions.register('move_block_changed', function() {
+  this.setOnChange(function() {
     if (this.getField('DROPDOWN_OPTIONS')) {
       var dropdownField = this.getField('DROPDOWN_OPTIONS');
       var dropdownValue = this.getFieldValue('DROPDOWN_OPTIONS');
@@ -73,11 +34,44 @@ Blockly.Extensions.register('move_block_warning_on_change', function() {
         dropdownField.forceRerender(); // Required instruction as it updates the dropdown visually 
         updateBlocks();
       }
+
     } else {
       this.setWarningText('Must have an input block.');
     }
   });
 });
+
+function updateDropdownOptions(dropdownField) {
+  var dropdownOptions = dropdownField.getOptions(false);
+
+  // Push unsaved positions into dropdown options
+  // TODO: Optmize this verification to reduce complexity
+  for (let [mapPosition, mapValue] of mapPositions) {
+    value_exists = false;
+
+    for (let [dropPosition, dropValue] of dropdownOptions) {
+      // If position already exists in dropdown options, break */
+      if (mapValue == dropValue) {
+        value_exists = true;
+        break;
+      }
+    }
+
+    if (!value_exists) {
+      dropdownOptions.push([mapPosition, mapValue]);
+    }
+  }
+
+  dropdownField.menuGenerator_ = dropdownOptions;
+}
+
+function updateBlocks() {
+Blockly.getMainWorkspace().getBlocksByType("move_to_position").forEach(function(block) {
+  var dropdownField = block.getField('DROPDOWN_OPTIONS');
+  updateDropdownOptions(dropdownField);
+});  
+}
+
   
 Blockly.defineBlocksWithJsonArray([
   /* Custom movement block */
@@ -97,8 +91,31 @@ Blockly.defineBlocksWithJsonArray([
     "colour": 8,
     "tooltip": "",
     "helpUrl": "",
-    // "mutator": "custom_movement_mutator",
-    "extensions": ["move_block_fill_options", "move_block_warning_on_change"]
+    "extensions": ["move_block_created", "move_block_changed"]
+  },
+  /* Custom pick object block */
+  {
+    "type": "pick_object",
+    "message0": "Pick object",
+    "inputsInline": false,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 50,
+    "tooltip": "",
+    "helpUrl": "",
+    "extensions": []
+  },
+  /* Custom release object block */
+  {
+    "type": "release_object",
+    "message0": "Release object",
+    "inputsInline": false,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 100,
+    "tooltip": "",
+    "helpUrl": "",
+    "extensions": []
   },
 ]);
 
@@ -106,11 +123,75 @@ const toolbox = {
   "kind": "flyoutToolbox",
   "contents": [
     {
+      "kind": "label",
+      "text": "Actions",
+    },
+    {
+      "kind": "button",
+      "text": "Manage Positions",
+      "callbackKey": "manage-positions"
+    },
+    {
+      "kind": "label",
+      "text": "Blocks",
+    },
+    {
       "kind": "block",
       "type": "move_to_position"
+    },
+    {
+      "kind": "block",
+      "type": "pick_object"
+    },
+    {
+      "kind": "block",
+      "type": "release_object"
     },
   ]
 }
 
-const blocklyDiv = document.getElementById('blockly-div');
-const workspace = Blockly.inject(blocklyDiv, {toolbox: toolbox});
+const blocklyDiv = document.getElementById('blockly-workspace');
+const workspace = Blockly.inject(blocklyDiv, {
+          toolbox: toolbox, zoom:
+          {controls: true,
+          wheel: true,
+          startScale: 1.5,
+          maxScale: 3,
+          minScale: 0.3,
+          scaleSpeed: 1.2,
+          pinch: true},
+          trashcan: true});
+
+workspace.registerButtonCallback("manage-positions", managePositions);
+
+function managePositions() {
+  const listModal = new bootstrap.Modal(document.getElementById('list-positions-modal')).show();
+  var positions = document.getElementById('positions-list');
+  var fill = "";
+
+  for (let [mapPosition, mapValue] of mapPositions) {
+    fill += '<a id="position-value" class="list-group-item">\
+              <div class="row">\
+                  <div id="position-name" class="col-6">' + mapPosition + '</div>\
+                  <div id="position-options" class="col-6">\
+                      <button type="button" class="btn btn-outline-primary btn-sm"><i class="fa-solid fa-pen"></i> Rename</button>\
+                      <button type="button" class="btn btn-outline-primary btn-sm"><i class="fa-solid fa-crosshairs"></i> Reposition</button>\
+                  </div>\
+              </div>\
+            </a>'
+  }
+
+  positions.innerHTML = fill;
+}
+
+var createPositionButton = document.getElementById("create-position-button");
+
+createPositionButton.addEventListener("click", function(event){
+  var createModal = document.getElementById('create-position-modal');
+  var modal = bootstrap.Modal.getInstance(createModal)
+  modal.hide();
+});
+
+function createPosition() {
+  console.log("Teste")
+}
--- js/simulation.js
+++ js/simulation.js
@@ -1,4 +1,5 @@
-/* 
+import Phaser from 'phaser';
+
 var config = {
     width: 800,
     height: 600,
@@ -18,15 +19,11 @@ var config = {
     }
 };
 
-var game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
 
 function preload ()
 {
     this.load.setBaseURL('http://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
 }
 
 function create ()
@@ -49,4 +46,3 @@ function create ()
 
     emitter.startFollow(logo);
 }
-*/
