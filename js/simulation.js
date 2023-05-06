cee287c8cae8e57ee6905693e21b55d9659061d9
Felipe Fronchetti <fronchettl@vcu.edu>
2023-05-05 22:52:13 -0400

    Upload map

--- js/simulation.js
+++ js/simulation.js
@@ -1,7 +1,7 @@
 
 var config = {
-    width: 600,
-    height: 600,
+    width: 640,
+    height: 640,
     parent: 'game-canvas',
     physics: {
         default: 'arcade',
@@ -23,35 +23,62 @@ const game = new Phaser.Game(config);
 
 function preload()
 {
-    this.load.image('robot', 'https://raw.githubusercontent.com/fronchetti/path-planning-web/main/assets/robot.png?token=GHSAT0AAAAAABQOGQYVKY42KLGBDY7C5U7IZCOAAKQ');
+    this.load.image('floor', 'https://i.imgur.com/4Qvn5sX.png');
+    this.load.spritesheet('robot', 'https://i.imgur.com/mb4zipF.png', { frameWidth: 32, frameHeight: 32 });
 }
 
 function create()
 {
     this.cursors = this.input.keyboard.createCursorKeys();
-    this.player = this.physics.add.image(100, 100, 'robot');
-    this.player.scale = 0.1;
-    this.player.setCollideWorldBounds(true);
+    this.robot = this.physics.add.sprite(160, 160, 'robot');
+    
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('robot', { start: 5, end: 6 }),
+        frameRate: 2,
+        repeat: -1
+      });
+      this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('robot', { start: 2, end: 3 }),
+        frameRate: 2,
+        repeat: -1
+      });
+      this.anims.create({
+        key: 'up',
+        frames: this.anims.generateFrameNumbers('robot', { start: 0, end: 1 }),
+        frameRate: 2,
+        repeat: -1
+      });
+      this.anims.create({
+        key: 'down',
+        frames: this.anims.generateFrameNumbers('robot', { start: 7, end: 8}),
+        frameRate: 2,
+        repeat: -1
+      });
+
+      this.robot.setCollideWorldBounds(true);
+      this.robot.setScale(2);
 }
 
 function update ()
 {
-    this.player.setVelocity(0);
+    this.robot.setVelocity(0);
 
-    if (this.cursors.left.isDown)
-    {
-        this.player.setVelocityX(-300);
+    if (this.cursors.left.isDown) {
+        this.robot.setVelocityX(-300);
+        this.robot.anims.play("left");
     }
-    else if (this.cursors.right.isDown)
-    {
-        this.player.setVelocityX(300);
+    else if (this.cursors.right.isDown) {
+        this.robot.setVelocityX(300);
+        this.robot.anims.play("right");
     }
-    else if (this.cursors.up.isDown)
-    {
-        this.player.setVelocityY(-300);
+    else if (this.cursors.up.isDown) {
+        this.robot.setVelocityY(-300);
+        this.robot.anims.play("up");
     }
-    else if (this.cursors.down.isDown)
-    {
-        this.player.setVelocityY(300);
+    else if (this.cursors.down.isDown) {
+        this.robot.setVelocityY(300);
+        this.robot.anims.play("down");
     }
 }
